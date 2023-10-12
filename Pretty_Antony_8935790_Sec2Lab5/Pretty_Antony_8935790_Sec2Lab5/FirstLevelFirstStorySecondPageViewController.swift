60ba40d30daf2c257fb8c6e0671ca8ee7f75ae2e
PrettyAntony <145509389+PrettyAntony@users.noreply.github.com>
2023-10-11 23:35:50 -0400

    Add files via upload

---
+++ Pretty_Antony_8935790_Sec2Lab5/Pretty_Antony_8935790_Sec2Lab5/FirstLevelFirstStorySecondPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstLevelFirstStorySecondPageViewController.swift
+//  Pretty_Antony_8935790_Sec2Lab5
+//
+//  Created by user234138 on 10/9/23.
+//
+
+import UIKit
+
+class FirstLevelFirstStorySecondPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pretty_Antony_8935790_Sec2Lab5/Pretty_Antony_8935790_Sec2Lab5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pretty_Antony_8935790_Sec2Lab5
+//
+//  Created by user234138 on 10/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
